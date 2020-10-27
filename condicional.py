375d4e42c91682ba2be63b26eceb2f8f99a2d1be
amendezb <a.mendez.b@hotmail.com>
2020-10-27 16:02:22 -0400

    Cambios de prueba2

---
+++ condicional.py
@@ -0,0 +1,11 @@
+x= int(input('primer numero: '))
+y= int(input('segundo numero: '))
+
+if x == y:
+    print('son iguales')
+elif x > y:
+    print('x mayor que y')
+elif x < y:
+    print('y mayor que x')
+else:
+    print('comparacion incorrecta')
